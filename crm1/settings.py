3f56acffc40e9ca821b98f6c6e9ef06f84c83247
Kenechi Ifeanyi <kenechiifeanyi@gmail.com>
2022-07-23 14:32:24 +0100

    Add files via upload

--- crm1/settings.py
+++ crm1/settings.py
@@ -131,5 +131,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'kayprogrammer1@gmail.com'
-EMAIL_HOST_PASSWORD = 'avanac7#'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
